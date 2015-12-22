34b42d809bc5cd2747e17ae18b425beaaba7b57f
Chloe Potter <cody.kendall21@gmail.com>
2015-12-22 06:58:26 -0800

    rudy methods

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,3 @@
+def make_address(street_address, city, state, zip)
+"You live at #{street_address}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+end
---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,2 @@
+def do_something(name, color, hair)
+end
