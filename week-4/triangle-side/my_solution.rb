0ffbd504db24bf848df15e860457b8eab64a0ab0
Chloe Potter <cody.kendall21@gmail.com>
2015-12-30 07:38:10 -0800

    finish 4.5

---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,12 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+  return false if (a == 0 || b == 0 || c == 0) 
+  if (a + b >= c) && (a + c >= b) && (b + c >= a)
+  	true
+  end
+end
+
