942681eaa6a3940ce85bab72c03421afa90cbef1
Chloe Potter <cody.kendall21@gmail.com>
2016-01-10 18:38:21 -0800

    update account groups

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -2,10 +2,8 @@
 #steps
 #1. get all names into an array
 #2. Run through names and place 5 into new array
-#3. If I want to get fancy do random people in different array
-#4. If one person leaves remove that person from array
 def cohort_generator(names)
-  	
+	total = []
 	cohort_1 = []
 	cohort_2 = []
 	cohort_3 = []
@@ -20,21 +18,23 @@ def cohort_generator(names)
 	cohort_12 = []
 	cohort_13 = []
 	cohort_14 = []
+	
+	names.each_slice(5){|names| total << names}
 
-	cohort_1 << names[0..3]
-	cohort_2 << names[4..7]
-	cohort_3 << names[8..11]
-	cohort_4 << names[12..15]
-	cohort_5 << names[16..19]
-	cohort_6 << names[20..23]
-	cohort_7 << names[24..27]
-	cohort_8 << names[28..31]
-	cohort_9 << names[32..35]
-	cohort_10 << names[36..39]
-	cohort_11 << names[40..43]
-	cohort_12 << names[44..47]
-	cohort_13 << names[48..51]
-	cohort_14 << names[52..55]
+	cohort_1 << total[1]
+	cohort_2 << total[2]
+	cohort_3 << total[3]
+	cohort_4 << total[4]
+	cohort_5 << total[5]
+	cohort_6 << total[6]
+	cohort_7 << total[7]
+	cohort_8 << total[8]
+	cohort_9 << total[9]
+	cohort_10 << total[10]
+	cohort_11 << total[11]
+	cohort_12 << total[12]
+	cohort_13 << total[13]
+	cohort_14 << total[14]
 end
 
 cohort_generator(["John Seo", "Aaron Hu", "Adam Pinsky", "Akeem Street",
