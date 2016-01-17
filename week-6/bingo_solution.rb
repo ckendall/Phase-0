3965bca8e1120d4dffb0095122e1a2f44245ef35
Chloe Potter <cody.kendall21@gmail.com>
2016-01-17 15:29:19 -0800

    bingo

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -140,12 +140,38 @@ new_game.play
 
 #Reflection
 #How difficult was pseudocoding this challenge? What do you think of your pseudocoding style?
+# Pseudocoding is always hard for me so I did spent a little more time on this and it felt great. I got to think about it more and I think my code came out a little more. I think that my pseudocoding style is not the greatest, but it is improving. I like being able to think about the program before coding, and it does seem to help.
+ 
 #What are the benefits of using a class for this challenge?
+# The benefits for using a class is that I am able to pass variables around much easier than if I  were to use just methods. It’s also its own object so if I need to give it to another part in code I could pass it to whatever I want as a whole.
+ 
 #How can you access coordinates in a nested array?
+# There are many ways to access coordinates in a nested array, but for this challenge I used two loop methods one was a map! and another with each_with_index. I needed two in order to access the inner loop.
+ 
 #What methods did you use to access and modify the array?
+# I used, as I said above, were map! to modify the array and each_with_index to see what index that I was in to access the elements.
+ 
 #Give an example of a new method you learned while reviewing the Ruby docs. Based on what you see in the docs, what purpose does it serve, and how is it called?
+# I used each_with_index and based on what I read the purpose of it is to run through elements in an array or hash and see the element and what index that element is at. For example I have an array I = [1,2,3]. To call the array with the method I would do this:
+#I.each_with_index do |element, index|
+ #               P element
+  #              P index
+#End
+ 
+#The output would go through each and be like this
+#1
+#0
+#2
+#1
+#3
+#2
+#The first number is the element and the second is the index.
+ 
 #How did you determine what should be an instance variable versus a local variable?
+# Pretty easy, I found what variable I needed in most methods and put that as an instance variable and the looping variables all needed to be local.
+ 
 #What do you feel is most improved in your refactored solution?
+# I feel like how I made my column assignment was the most improved for just the static. I think this because it give is more flexibility if something needed to change.
 
 
 
